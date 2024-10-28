30699d4c7afa366f75a1b3b06f805aafff45e92b
fridayezenwa <35106164+fridayezenwa@users.noreply.github.com>
2024-10-28 03:31:52 -0400

    Added nodeapp directory

--- nodeapp/app.js
+++ nodeapp/app.js
@@ -1,10 +1,10 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
 
-app.get("/",(req,res)=>{
-    res.send("Welcome to my node.js app")
-})
+app.get("/", (req, res) => {
+  res.send("Welcome to my node.js app);
+});
 
-app.listen(8080,()=>{
-    console.log("Server is running")
-})
+app.listen(8080, () => {
+  console.log("Server is up and running");
+});
